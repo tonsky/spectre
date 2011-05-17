049d9d2f03a525276d7c374fc0c4f563d51f31e6
Nikita Prokopov <prokopov@gmail.com>
2011-05-17 18:14:25 +0700

    ChMock to accept byte[] to exclude encoding hell in asyncio tests

--- src/java/asyncio/test/ChMock.java
+++ src/java/asyncio/test/ChMock.java
@@ -10,15 +10,30 @@ import java.nio.channels.*;
  */
 class ChMock implements ReadableByteChannel {
   public boolean _open = true;
-  public String[] _chunks;
+  public byte[][] _chunks;
   public boolean _blocking;
   public int _chunksIdx = 0, _strPos = 0;
   
-  public ChMock(String[] chunks, boolean blocking) { this._chunks = chunks; this._blocking = blocking; }
+  public ChMock(Object[] chunks, boolean blocking) { 
+    try {
+      this._chunks = new byte[chunks.length][];
+      for(int i=0;i<chunks.length;++i) {
+        if (chunks[i] instanceof String)      
+          _chunks[i] = ((String)chunks[i]).getBytes("UTF-8");
+        else if (chunks[i] instanceof byte[])
+          _chunks[i] = (byte[])chunks[i];
+        else
+          throw Err.make("ChMock accepts only ASCII String or byte[], not " + chunks[i].getClass()).val;
+      }
+      this._blocking = blocking;
+    } catch (java.io.UnsupportedEncodingException e) {
+      throw Err.make("What, no UTF-8?", e).val;
+    }
+  }
   
   public int read(ByteBuffer dst) {
     if (_chunksIdx >= _chunks.length) return -1; // no more data
-    while (_strPos >= _chunks[_chunksIdx].length()) { // no more data in current chunk
+    while (_strPos >= _chunks[_chunksIdx].length) { // no more data in current chunk
       _chunksIdx++;
       _strPos = 0;
       if (_chunksIdx >= _chunks.length) return -1;
@@ -26,8 +41,8 @@ class ChMock implements ReadableByteChannel {
     }
     
     // otherwise send what rest of the current chunk
-    int toCopy = Math.min(dst.remaining(), _chunks[_chunksIdx].length() - _strPos);
-    dst.put(_chunks[_chunksIdx].getBytes(), _strPos, toCopy);
+    int toCopy = Math.min(dst.remaining(), _chunks[_chunksIdx].length - _strPos);
+    dst.put(_chunks[_chunksIdx], _strPos, toCopy);
     _strPos += toCopy;
     return toCopy;
   }
